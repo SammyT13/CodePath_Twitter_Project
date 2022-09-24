396513d44cf1e266e70cab147ba5f0715bfc32df
Sammy Torres <91271419+SammyT13@users.noreply.github.com>
2022-09-23 23:39:21 -0400

    Updated Logout feature.  Changed Launch Screen image.

---
+++ Twitter/Twitter/HomeTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Sammy Torres II on 9/23/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+// This is linked to the 'Home' Screen and 'Logout' button
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+        // this is the logout function already created
+        TwitterAPICaller.client?.logout()
+        // this dismisses the Home screen after clicking logout
+        self.dismiss(animated: true, completion: nil)
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
